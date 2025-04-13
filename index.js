9d3ed001f1cccc481853d46afc976f0460d45393
Heather <heatherboden3@gmail.com>
2025-04-13 11:14:49 -0400

    removed unnecessary code

--- index.js
+++ index.js
@@ -16,33 +16,9 @@ const port = process.env.PORT || "6004";
 const dbName = 'portfoliodb';
 
 //set up application template engine
-app.set("views", path.join(__dirname, "views")); //the first "views" is the setting name
-//the second value above is the path : __dirname/ views
+app.set("views", path.join(__dirname, "views")); 
 app.set("view engine", "pug");
 
-//skills end point API
-app.get("/getAllSkills"),(request, response) =>{
-    
-}
-
-
-
-// app.get('/skills', (request, response )=>{
-//     response.render('skills');
-// });
-// app.get("/projects", (request, response)=>{
-//     response.render('projects');
-// })
-// app.get("/", async (request, response) =>{
-//     try {
-//         //fetch projects and skills concurrently
-//         const[projects, skills] = await Promise.all([getProjects(), getSkills()]);
-//         response.render("index", {projects, skills});
-//     } catch(error){
-//         console.error("Error fetching portfolio data: ", error)
-//         response.status(500).send("Internal Server Error");
-//     }
-// });
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
